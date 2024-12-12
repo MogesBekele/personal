3f88e38e436cbc48820898e638bad2930983728f
MogesBekele <mogesbekele32@gmail.com>
2024-12-12 17:04:57 +0300

    add two projects

--- personal/js/index.js
+++ personal/js/index.js
@@ -90,12 +90,12 @@ const activePortfolio = ()=>{
 }
 
 arrowRight.addEventListener('click', () =>{
-if(index<4){
+if(index<6){
   index++;
   arrowLeft.classList.remove('disabled')
 }
 else{
-  index = 5
+  index = 7
   arrowRight.classList.add('disabled')
 }
 activePortfolio()
